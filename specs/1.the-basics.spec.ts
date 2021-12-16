e32e8b0c351b6f9b82d335d0b02bb34cd2d856da
Piotr Nowak <p.nowak2@gmail.com>
2021-12-16 14:00:28 +0100

    done

--- specs/1.the-basics.spec.ts
+++ specs/1.the-basics.spec.ts
@@ -1,10 +1,45 @@
 import { Paginator } from "../src/paginator";
 
-describe("1 Basic Types", () => {
-    describe("Boolean", () => {
-        it("should declare it", () => {
-            const p = Paginator.fromPagination();
-            expect(p).toBeInstanceOf(Paginator);
-        })
+describe("Pagination", () => {
+    describe("Initialization", () => {
+        it("should init without config", () => {
+            const paginator = Paginator.create();
+
+            expect(paginator.getTotalItems()).toBe(10);
+            expect(paginator.getPageSize()).toBe(10);
+            expect(paginator.getCurrentPage()).toBe(1);
+            expect(paginator.getPageWindowindowSize()).toBe(5);
+        });
+
+        it("should init with empty config", () => {
+            const paginator = Paginator.create({});
+
+            expect(paginator.getTotalItems()).toBe(10);
+            expect(paginator.getPageSize()).toBe(10);
+            expect(paginator.getCurrentPage()).toBe(1);
+            expect(paginator.getPageWindowindowSize()).toBe(5);
+        });
+
+        it("should init with provided config", () => {
+            const paginator = Paginator.create({
+                totalItems: 5,
+                pageSize: 5,
+                currentPage: 3,
+                pageWindowSize: 3
+            });
+
+            expect(paginator.getTotalItems()).toBe(5);
+            expect(paginator.getPageSize()).toBe(5);
+            expect(paginator.getCurrentPage()).toBe(3);
+            expect(paginator.getPageWindowindowSize()).toBe(3);
+        });
+
+        it("should not accept invalid arguments", () => {
+            expect(() => {
+                Paginator.create({
+                    totalItems: -1
+                });
+            }).toThrow('total items cannot be negative');
+        });
     });
-})
+});
--- src/paginator.ts
+++ src/paginator.ts
@@ -1,14 +1,51 @@
-export class Paginator {
-    constructor(
-        private totalItems: number,
-        private pageSize: number,
-        private currentPage: number,
-        private pageWindowSize: number) { 
-            
-        }
+export interface PaginatorConfig {
+    totalItems?: number;
+    pageSize?: number;
+    currentPage?: number;
+    pageWindowSize?: number;
+}
 
-    public static fromPagination(): Paginator {
-        return new Paginator(0, 0 , 0, 0);
+const DefaultConfig: PaginatorConfig = {
+    totalItems: 10,
+    pageSize: 10,
+    currentPage: 1,
+    pageWindowSize: 5
+}
+
+export class Paginator {
+    private constructor(
+        private totalItems: number = 10,
+        private pageSize: number = 10,
+        private currentPage: number = 1,
+        private pageWindowSize: number = 5) {
     }
 
+    public static create(config: PaginatorConfig = DefaultConfig): Paginator {
+        if (config.totalItems && config.totalItems < 0) {
+            throw new Error('total items cannot be negative');
+        }
+
+        return new Paginator(
+            config.totalItems,
+            config.pageSize,
+            config.currentPage,
+            config.pageWindowSize
+        );
+    }
+
+    getTotalItems(): number {
+        return this.totalItems;
+    }
+
+    getPageSize(): number {
+        return this.pageSize;
+    }
+
+    getCurrentPage(): number {
+        return this.currentPage;
+    }
+
+    getPageWindowindowSize(): number {
+        return this.pageWindowSize;
+    }
 }
